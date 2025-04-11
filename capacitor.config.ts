144b220e9faf8a6d35265689387e82a41ae0fc3a
ZhanboGong <z.gong.11@student.scu.edu.au>
2025-04-11 19:22:09 +0800

    Initial setup of team project（1）
    Perform a series of initial Settings for the project, including creating all the required tab pages and setting up routes, and changing the default theme of the app (Due to the size limit of the uploaded file, this section is divided into multiple parts for uploading）

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'A3_Team',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: './tab5.page.html',
+  standalone: false,
+  styleUrls: ['./tab5.page.scss'],
+})
+export class Tab5Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

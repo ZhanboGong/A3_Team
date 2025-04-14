aec9272c46b6c45fb0a38964680d26611752c493
ZhanboGong <z.gong.11@student.scu.edu.au>
2025-04-14 17:38:22 +0800

    Add dataValidation method
    Contains fill verification except for Special Note, uniqueness verification for ID and Name, and positive integer verification for price and quantity

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -8,15 +8,11 @@ import { Observable } from "rxjs";
 })
 export class InventoryService {
     // Assessment server endpoint
-    private url = 'https://prog2005.it.scu.edu.au/ArtGalley';
+    private url = 'https://prog2005.it.scu.edu.au/ArtGalley/';
 
     constructor(private http: HttpClient) {
     }
 
-    // Data Validation(To be development)
-    dataValidation() {
-
-    }
 
     // Gets all records in the url
     getAllItems(): Observable<Item[]> {
@@ -25,7 +21,7 @@ export class InventoryService {
 
     // Look for records by name
     getItemByName(name: string): Observable<Item[]> {
-        return this.http.get<Item[]>(this.url + "/" + name);
+        return this.http.get<Item[]>(this.url + name);
     }
 
     // Add new Item
@@ -42,5 +38,59 @@ export class InventoryService {
     deleteItem(name: string): Observable<any> {
         return this.http.delete(`${this.url}${name}`);
     }
+
+    // Used for data validation in the case of additions, updates, etc
+    dataValidation(newItem: Item, allItems: Item[]): string | null {
+        // Verify that the Item ID is an integer
+        if (typeof newItem.item_id !== 'number') {
+            console.error('Validation Error: item_id must be a number.');
+            return "Item ID Must be a Number!";
+        }
+
+        // Verify that the Item ID must be Unique
+        if (allItems.some(item => item.item_id === newItem.item_id)) {
+            console.error('Validation Error: item_id must be unique.');
+            return "Item ID Must be Unique!";
+        }
+
+        // Verify that the Item Name cannot be Empty
+        if (!newItem.item_name || newItem.item_name.trim() === "") {
+            console.error("Validation Error: item_name cannot be empty.");
+            return "Item Name cannot be Empty!";
+        }
+
+        // Verify that the Item Name must be Unique
+        if (allItems.some(item => item.item_name === newItem.item_name)) {
+            console.error("Validation Error: item_name must be unique");
+            return "Item Name Must be Unique!";
+        }
+
+        // Verify that the Supplier Name cannot be Empty
+        if (!newItem.supplier_name || newItem.supplier_name.trim() === "") {
+            console.error("Validation Error: supplier_name cannot be Empty!");
+            return "Supplier Name cannot be Empty!";
+        }
+
+        // Verify that the Radio Input cannot be empty
+        if (!newItem.category || !newItem.stock_status || !newItem.featured_item) {
+            console.error("Validation Error: Radio Input cannot be Empty");
+            return "Radio Input cannot be Empty!";
+        }
+
+        // Verify that the Item Price must be a positive integer
+        if (newItem.price < 0 || !(newItem.price % 1 === 0)) {
+            console.error("Validation Error: price must be a positive integer.");
+            return "Item Price must be a positive integer!";
+        }
+
+        // Verify that the Item Quantity must be a positive integer
+        if (newItem.quantity < 0 || !(newItem.quantity % 1 === 0)) {
+            console.error("Validation Error: quantity must be a positive integer.");
+            return "Item Quantity must be a positive integer!";
+        }
+
+        return null;
+    }
+
 
 }

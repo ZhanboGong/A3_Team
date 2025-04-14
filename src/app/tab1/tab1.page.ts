1cf27e23c0fc6c30defc4d5c9e7cb5f0c41fa976
ZhanboGong <z.gong.11@student.scu.edu.au>
2025-04-14 21:07:37 +0800

    Add the add and Delete methods
    The Add and Delete methods are used to add and delete related items, where the delete method is found by itemName

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../services/inventory.service';
+import { Item } from '../models/item.model';
 
 @Component({
   selector: 'app-tab1',
@@ -6,8 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
   standalone: false,
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  items: Item[] = [];
+  constructor(private inventoryService: InventoryService) { }
+  ngOnInit(): void {
+    this.getItems();
+  }
 
-  constructor() {}
-
+  getItems() {
+    this.inventoryService.getAllItems().subscribe({
+      next: (items: Item[]) => {
+        this.items = items;
+      },
+      error: (error: any) => {
+        console.error('Error fetching items:', error);
+      }
+    });
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryService } from '../services/inventory.service';
+import { Item } from '../models/item.model';
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +8,80 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  items: Item[] = [];
+  newItem: Item = {
+    item_id: 0,
+    item_name: '',
+    category: 'Electronics',
+    quantity: 0,
+    price: 0,
+    supplier_name: '',
+    stock_status: 'In Stock',
+    featured_item: 0,
+    special_note: ''
+  };
+  sidebarView: 'overview' | 'modify' | 'add' = 'overview';
 
-  constructor() {}
+  constructor(private inventoryService: InventoryService) { }
+  ngOnInit(): void {
+    this.getItems();
+  }
+  getItems() {
+    this.inventoryService.getAllItems().subscribe({
+      next: (items: Item[]) => {
+        this.items = items;
+      },
+      error: (error: any) => {
+        console.error('Error fetching items:', error);
+      }
+    });
+  }
 
+  addNewItem() {
+    const validationMessage = this.inventoryService.dataValidation(this.newItem, this.items);
+    if (validationMessage) {
+      // Add promot tips
+      // this.message = validationMessage;
+      return;
+    }
+    this.inventoryService.addRecord(this.newItem).subscribe({
+      next: () => {
+        this.getItems();
+        this.newItem = {
+          item_id: 0,
+          item_name: '',
+          category: 'Electronics',
+          quantity: 0,
+          price: 0,
+          supplier_name: '',
+          stock_status: 'In Stock',
+          featured_item: 0,
+          special_note: ''
+        };
+      },
+      error: (error: any) => {
+        console.error('Error adding item:', error);
+      }
+    });
+  }
+
+  // 这里需要添加删除提示
+  deleteItem(name: string) {
+    this.inventoryService.deleteItem(name).subscribe({
+      next: () => {
+        this.getItems();
+      },
+      error: (error: any) => {
+        console.error('Error deleting item:', error);
+      }
+    });
+  }
+
+  switchView(selectedView: 'overview' | 'modify' | 'add') {
+    this.sidebarView = selectedView;
+    if (selectedView === 'overview' || selectedView === 'modify') {
+      this.getItems();
+    }
+  }
 }

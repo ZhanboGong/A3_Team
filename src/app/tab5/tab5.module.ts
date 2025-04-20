0fbb0b9cf2adf4e2bbd2cf88cbc9397467921001
JingyuZhang <j.zhang.73@student.scu.edu.au>
2025-04-20 14:10:51 +0800

    Update Privacy Page

--- src/app/tab5/tab5.module.ts
+++ src/app/tab5/tab5.module.ts
@@ -1,18 +1,18 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
+import { Tab5Page } from './tab5.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab5PageRoutingModule } from './tab5-routing.module';
 
-import { Tab5Page } from './tab5.page';
-
 @NgModule({
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
+    ExploreContainerComponentModule,
     Tab5PageRoutingModule
   ],
   declarations: [Tab5Page]
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab5',
-  templateUrl: './tab5.page.html',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss'],
   standalone: false,
-  styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
+  helpModelStatus = false;
+  privacyPolicies = [
+    { title: 'Information Collection', icon: 'document-text', expanded: false },
+    { title: 'Data Usage', icon: 'analytics', expanded: false },
+    { title: 'Data Storage', icon: 'server', expanded: false },
+    { title: 'Third-Party Sharing', icon: 'people', expanded: false },
+    { title: 'User Rights', icon: 'shield-checkmark', expanded: false }
+  ];
+  
+  lastUpdated = 'April 20, 2025';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    console.log('Privacy page initialized');
   }
 
+  toggleSection(index: number) {
+    this.privacyPolicies[index].expanded = !this.privacyPolicies[index].expanded;
+  }
+
+  navigateToHome() {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
+
+  /**
+   * modal control: Set help modal's display state to true(open)
+   */
+  openHelpModel() {
+    this.helpModelStatus = true;
+    console.log("Privacy Page Help open");
+  }
+
+  closeHelpModel() {
+    this.helpModelStatus = false;
+    console.log("Privacy Page Help close");
+  }
 }
